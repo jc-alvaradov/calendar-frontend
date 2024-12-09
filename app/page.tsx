b067a05518dbc5b67ffe99683d06fe884f6e9c91
Juan Carlos Alvarado <jc.alvaradov93@gmail.com>
2024-12-08 23:40:36 -0300

    fix: adds cache invalidation for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Scheduler, Instagram } from "@/components/features";
 import { EventType } from "@/types/event";
 import { query } from "./service";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const events: EventType[] = await query();
 
